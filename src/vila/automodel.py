369367ec73b62748a522c448d4ec398ebe2d2b9e
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2022-08-05 14:09:46 -0400

    Update autoclass to remove the transformer version constraints  (#27)
    * Adopting a new way for autoclasses
    
    * remove the pip reqs
    
    * Add CI
    
    * install poppler in CI
    
    * sudo install poppler in CI

--- src/vila/automodel.py
+++ src/vila/automodel.py
@@ -1,28 +1,56 @@
-from .models import HierarchicalModelConfig, HierarchicalModelForTokenClassification
-
-from transformers import (
-    CONFIG_MAPPING,
-    MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING,
-    MODEL_NAMES_MAPPING,
-    TOKENIZER_MAPPING,
+from .models import (
+    HierarchicalModelConfig,
+    SimpleHierarchicalModel,
+    HierarchicalModelForTokenClassification,
 )
-from transformers.models.auto.modeling_auto import auto_class_factory
-from transformers import BertTokenizer, BertTokenizerFast, AutoTokenizer
+import warnings
 
-CONFIG_MAPPING.update([("hierarchical_model", HierarchicalModelConfig)])
-MODEL_NAMES_MAPPING.update([("hierarchical_model", "HierarchicalModel")])
-TOKENIZER_MAPPING.update(
-    [
-        (HierarchicalModelConfig, (BertTokenizer, BertTokenizerFast)),
-    ]
-)
+try:
+    from transformers import (
+        CONFIG_MAPPING,
+        MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING,
+        MODEL_NAMES_MAPPING,
+        TOKENIZER_MAPPING,
+    )
 
-MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING.update(
-    [(HierarchicalModelConfig, HierarchicalModelForTokenClassification)]
-)
+    from transformers.models.auto.modeling_auto import auto_class_factory
+    from transformers import BertTokenizer, BertTokenizerFast, AutoTokenizer
 
-AutoModelForTokenClassification = auto_class_factory(
-    "AutoModelForTokenClassification",
-    MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING,
-    head_doc="token classification",
-)
+    warnings.warn("Currently using an old version of transformers for configuring the AutoModel. Consider updating your transformers to the latest version(>=4.21.x).")
+    CONFIG_MAPPING.update([("hierarchical_model", HierarchicalModelConfig)])
+    MODEL_NAMES_MAPPING.update([("hierarchical_model", "HierarchicalModel")])
+    TOKENIZER_MAPPING.update(
+        [
+            (HierarchicalModelConfig, (BertTokenizer, BertTokenizerFast)),
+        ]
+    )
+
+    MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING.update(
+        [(HierarchicalModelConfig, HierarchicalModelForTokenClassification)]
+    )
+
+    AutoModelForTokenClassification = auto_class_factory(
+        "AutoModelForTokenClassification",
+        MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING,
+        head_doc="token classification",
+    )
+
+except:
+    from transformers import (
+        AutoConfig,
+        AutoModel,
+        AutoTokenizer,
+        AutoModelForTokenClassification,
+        BertTokenizer,
+        BertTokenizerFast,
+    )
+    from transformers.models.auto.configuration_auto import CONFIG_MAPPING_NAMES
+
+    AutoConfig.register("hierarchical_model", HierarchicalModelConfig)
+    CONFIG_MAPPING_NAMES["hierarchical_model"] = HierarchicalModelConfig.__name__
+    # An issue in https://github.com/huggingface/transformers/pull/18491
+    AutoTokenizer.register(HierarchicalModelConfig, BertTokenizer, BertTokenizerFast)
+    AutoModel.register(HierarchicalModelConfig, SimpleHierarchicalModel)
+    AutoModelForTokenClassification.register(
+        HierarchicalModelConfig, HierarchicalModelForTokenClassification
+    )
